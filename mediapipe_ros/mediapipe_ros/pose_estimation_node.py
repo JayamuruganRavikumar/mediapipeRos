90550d5381ef46d2d42f0a1a5c76e0c85e64f4ec
Jay <jayamurugancbe@gmail.com>
2024-07-15 01:23:52 +0200

    gpu support test

--- mediapipe_ros/mediapipe_ros/pose_estimation_node.py
+++ mediapipe_ros/mediapipe_ros/pose_estimation_node.py
@@ -1,35 +1,18 @@
 import rclpy
 import cv2
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.framework.formats import landmark_pb2
+
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from rclpy.node import Node
 from mediapipe_msg.msg import PoseList
-from mediapipe.python.solutions.pose import PoseLandmark
+#from mediapipe.python.solutions.pose import PoseLandmark
 from sensor_msgs.msg import Image
 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
-pose_name = [
-    "NOSE", "LEFT_EYE_INNER",
-    "LEFT_EYE", "LEFT_EYE_OUTER",
-    "RIGHT_EYE_INNER", "RIGHT_EYE",
-    "RIGHT_EYE_OUTER", "LEFT_EAR",
-    "RIGHT_EAR", "MOUTH_LEFT",
-    "MOUTH_RIGHT",  "LEFT_SHOULDER",
-    "RIGHT_SHOULDER","LEFT_ELBOW",
-    "RIGHT_ELBOW", "LEFT_WRIST",
-    "RIGHT_WRIST", "LEFT_PINKY",
-    "RIGHT_PINKY", "LEFT_INDEX",
-    "RIGHT_INDEX", "LEFT_THUMB",
-    "RIGHT_THUMB", "LEFT_HIP",
-    "RIGHT_HIP",  "LEFT_KNEE",
-    "RIGHT_KNEE", "LEFT_ANKLE",
-    "RIGHT_ANKLE", "LEFT_HEEL",
-    "RIGHT_HEEL", "LEFT_FOOT_INDEX",
-    "RIGHT_FOOT_INDEX"
-]
 
 class PosePublisher(Node):
 
@@ -39,8 +22,47 @@ class PosePublisher(Node):
         self.image_publisher=self.create_publisher(Image, '/processed/image', 10)
         self.subscription = self.create_subscription(Image, '/rgb/image_raw', self.getrgb_callback, 10)
         self.subscription = self.create_subscription(Image, '/depth_to_rgb/image_raw', self.getdepth_callback, 10)
+
         self.model_path = "model/pose_landmarker_full.task"
+        self.num_poses = 1
+        self.min_pose_detection_confidence = 0.5
+        self.min_pose_presence_confidence = 0.5
+        self.min_tracking_confidence = 0.5
+        self.to_window = None
+        self.last_timestamp_ms = 0
         self.bridge = CvBridge()
+
+        self.base_options = python.BaseOptions(model_asset_path=self.model_path,
+                                               delegate=python.BaseOptions.Delegate.GPU)
+        self.options = vision.PoseLandmarkerOptions(
+            base_options=self.base_options,
+            running_mode=vision.RunningMode.LIVE_STREAM,
+            num_poses=self.num_poses,
+            min_pose_detection_confidence=self.min_pose_detection_confidence,
+            min_pose_presence_confidence=self.min_pose_presence_confidence,
+            min_tracking_confidence=self.min_tracking_confidence,
+            output_segmentation_masks=False,
+            result_callback=self.print_result
+        )
+        self.pose_name = [
+            "NOSE", "LEFT_EYE_INNER",
+            "LEFT_EYE", "LEFT_EYE_OUTER",
+            "RIGHT_EYE_INNER", "RIGHT_EYE",
+            "RIGHT_EYE_OUTER", "LEFT_EAR",
+            "RIGHT_EAR", "MOUTH_LEFT",
+            "MOUTH_RIGHT",  "LEFT_SHOULDER",
+            "RIGHT_SHOULDER","LEFT_ELBOW",
+            "RIGHT_ELBOW", "LEFT_WRIST",
+            "RIGHT_WRIST", "LEFT_PINKY",
+            "RIGHT_PINKY", "LEFT_INDEX",
+            "RIGHT_INDEX", "LEFT_THUMB",
+            "RIGHT_THUMB", "LEFT_HIP",
+            "RIGHT_HIP",  "LEFT_KNEE",
+            "RIGHT_KNEE", "LEFT_ANKLE",
+            "RIGHT_ANKLE", "LEFT_HEEL",
+            "RIGHT_HEEL", "LEFT_FOOT_INDEX",
+            "RIGHT_FOOT_INDEX"
+        ]
 
     #callback function for depth camera    
     def getdepth_callback(self, msg):
@@ -68,20 +90,50 @@ class PosePublisher(Node):
         except Exception as e:
             self.get_logger().error(f"Error form rgb camera: {e}")
 
+    def draw_landmarks_on_image(self, rgb_image, detection_result):
+        pose_landmarks_list = detection_result.pose_landmarks
+        annotated_image = np.copy(rgb_image)
+
+        # Loop through the detected poses to visualize.
+        for idx in range(len(pose_landmarks_list)):
+            pose_landmarks = pose_landmarks_list[idx]
+
+            pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+            pose_landmarks_proto.landmark.extend([
+                landmark_pb2.NormalizedLandmark(
+                    x=landmark.x,
+                    y=landmark.y,
+                    z=landmark.z) for landmark in pose_landmarks
+            ])
+            mp.solutions.drawing_utils.draw_landmarks(
+                annotated_image,
+                pose_landmarks_proto,
+                mp.solutions.pose.POSE_CONNECTIONS,
+                mp.solutions.drawing_styles.get_default_pose_landmarks_style())
+        return annotated_image
+
+    def print_result(self, detection_result: vision.PoseLandmarkerResult, output_image: mp.Image,
+                     timestamp_ms: int):
+        if timestamp_ms < self.last_timestamp_ms:
+            return
+        self.last_timestamp_ms = timestamp_ms
+        # print("pose landmarker result: {}".format(detection_result))
+        self.to_window = cv2.cvtColor(
+            self.draw_landmarks_on_image(output_image.numpy_view(), detection_result), cv2.COLOR_RGB2BGR)
+
     #compare depth and rgb image
     def compare_depth(self, image, depth):
 
         poselist = PoseList() 
+
+        with vision.PoseLandmarker.create_from_options(self.options) as landmarker:
         
-        with mp_pose.Pose(
-               min_detection_confidence=0.5,
-               min_tracking_confidence=0.5) as pose:
-            image.flags.writeable = False
-            results = pose.process(image)
             # Draw the pose annotation on the image.
-            image.flags.writeable = True
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            mp_drawing.draw_landmarks( image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            mp_image = mp.Image(
+                image_format=mp.ImageFormat.SRGB,
+                data=cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            timestamp_ms = int(cv2.getTickCount() / cv2.getTickFrequency() * 1000)
+            results = landmarker.detect_async(mp_image, timestamp_ms)
             h, w, c = image.shape
 
             if results.pose_landmarks != None:
@@ -90,7 +142,7 @@ class PosePublisher(Node):
                     #check for wrist
                     if 15 <= ids < 23:
                         cx,cy = pose_landmarks.x*w, pose_landmarks.y*h
-                        poselist.human_pose[index].name = pose_name[ids]
+                        poselist.human_pose[index].name =self.pose_name[ids]
                         poselist.human_pose[index].x = cx
                         poselist.human_pose[index].y = cy
                         poselist.human_pose[index].z = float(depth[int(cy),int(cx)])
@@ -101,7 +153,7 @@ class PosePublisher(Node):
                 index = 0
                 for ids, pose_landmarks in enumerate(results.pose_landmarks.landmark):
                     if 15 <= ids < 23:
-                        poselist.human_pose[index].name = pose_name[ids]
+                        poselist.human_pose[index].name = self.pose_name[ids]
                         poselist.human_pose[index].x = 0.0
                         poselist.human_pose[index].y = 0.0
                         poselist.human_pose[index].z = 0.0
